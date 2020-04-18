ad5dfa5c9c1d3ebc6cee2c6aa7c9a6671ad5b475
Vjaceslavs MArmiss <vjaceslavs.marmiss@gmail.com>
2020-04-18 23:37:01 +0200

    Project cleanup

--- js/script.js
+++ js/script.js
@@ -31,4 +31,48 @@ window.addEventListener("DOMContentLoaded", function(){
             }
         }
     });
+
+    //Timer
+    
+    let deadLine = "2020-04-18";
+    
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()), 
+          seconds = Math.floor((t / 1000) % 60),
+          minutes = Math.floor((t / 1000 / 60) % 60),
+          hours = Math.floor((t / (1000 * 60 * 60)));
+     
+        return {
+          total: t,
+          hours: hours,
+          minutes: minutes,
+          seconds: seconds,
+        };
+      }
+    function setClock(id,endTime){
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+                
+            
+
+            function updateClock(){                     //Updating the time
+                let t = getTimeRemaining(endTime);
+                hours.textContent = t.hours;
+                minutes.textContent = t.minutes;          
+                seconds.textContent = t.seconds;
+                if(t.total <= 0 ){
+                    clearInterval(timeInterval);
+                    hours.textContent = "00";
+                    minutes.textContent = "00";          
+                    seconds.textContent = "00";
+                }
+                
+            }
+
+    }
+
+    setClock('timer', deadLine);
 });
