6d2886ac91f1bac024127d3e22efa6d1df35d7df
Vjaceslavs MArmiss <vjaceslavs.marmiss@gmail.com>
2020-04-22 13:45:11 +0200

    Added slider

--- js/script.js
+++ js/script.js
@@ -1,150 +1,206 @@
-window.addEventListener("DOMContentLoaded", function(){
+window.addEventListener('DOMContentLoaded', function() {
+
     'use strict';
     let tab = document.querySelectorAll('.info-header-tab'),
         info = document.querySelector('.info-header'),
         tabContent = document.querySelectorAll('.info-tabcontent');
 
-
-    function hideTabContent(a){
-        for(let i = a; i < tabContent.length;i++){
+    function hideTabContent(a) {
+        for (let i = a; i < tabContent.length; i++) {
             tabContent[i].classList.remove('show');
             tabContent[i].classList.add('hide');
-
-
         }
     }
+
     hideTabContent(1);
-    function showTabContent(b){
-        if(tabContent[b].classList.contains('hide')){
+
+    function showTabContent(b) {
+        if (tabContent[b].classList.contains('hide')) {
             tabContent[b].classList.remove('hide');
             tabContent[b].classList.add('show');
         }
     }
-    info.addEventListener('click',function(event){
+
+    info.addEventListener('click', function(event) {
         let target = event.target;
-        if(target && target.classList.contains('info-header-tab')){
-            for(let i = 0;i <tab.length;i++){
-                if(target==tab[i]){
+        if (target && target.classList.contains('info-header-tab')) {
+            for(let i = 0; i < tab.length; i++) {
+                if (target == tab[i]) {
                     hideTabContent(0);
                     showTabContent(i);
+                    break;
                 }
             }
         }
+
     });
 
-    //Timer
-    
-    let deadLine = "2020-04-18";
-    
+    // Timer 
+
+    let deadline = '2018-11-21';
+
     function getTimeRemaining(endtime) {
-        let t = Date.parse(endtime) - Date.parse(new Date()), 
-          seconds = Math.floor((t / 1000) % 60),
-          minutes = Math.floor((t / 1000 / 60) % 60),
-          hours = Math.floor((t / (1000 * 60 * 60)));
-     
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+        seconds = Math.floor((t/1000) % 60),
+        minutes = Math.floor((t/1000/60) % 60),
+        hours = Math.floor((t/(1000*60*60)));
+
         return {
-          total: t,
-          hours: hours,
-          minutes: minutes,
-          seconds: seconds,
+            'total' : t,
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds
         };
-      }
-    function setClock(id,endTime){
+    }
+
+    function setClock(id, endtime) {
         let timer = document.getElementById(id),
             hours = timer.querySelector('.hours'),
             minutes = timer.querySelector('.minutes'),
             seconds = timer.querySelector('.seconds'),
             timeInterval = setInterval(updateClock, 1000);
-                
             
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
 
-            function updateClock(){                     //Updating the time
-                let t = getTimeRemaining(endTime);
-                hours.textContent = t.hours;
-                minutes.textContent = t.minutes;          
-                seconds.textContent = t.seconds;
-                if(t.total <= 0 ){
-                    clearInterval(timeInterval);
-                    hours.textContent = "00";
-                    minutes.textContent = "00";          
-                    seconds.textContent = "00";
-                }
-                
+            function addZero(num){
+                        if(num <= 9) {
+                            return '0' + num;
+                        } else return num;
+                    };
+
+            hours.textContent = addZero(t.hours);
+            minutes.textContent = addZero(t.minutes);
+            seconds.textContent = addZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
             }
+        }
 
     }
 
-    setClock('timer', deadLine);
-    //Modal window
+    setClock('timer', deadline);
+
+    // Modal
 
     let more = document.querySelector('.more'),
         overlay = document.querySelector('.overlay'),
-        close = document.querySelector(".popup-close");
+        close = document.querySelector('.popup-close');
 
-        more.addEventListener('click',function(){
-            overlay.style.display = 'block';
-            this.classList.add("more-splash");
-            document.body.style.overflow = 'hidden';
-
-        });
-
-        close.addEventListener('click',function(){
-            overlay.style.display = 'none';
-            more.classList.remove('more-splash');
-            document.body.style.overflow = '';
-
-
-
+    more.addEventListener('click', function() {
+        overlay.style.display = 'block';
+        this.classList.add('more-splash');
+        document.body.style.overflow = 'hidden';
     });
 
-        //Contact form
+    close.addEventListener('click', function() {
+        overlay.style.display = 'none';
+        more.classList.remove('more-splash');
+        document.body.style.overflow = '';
+    });
 
-        let message = {
-            loading: 'Loading',
-            success: 'Thank you!We will contact you soon',
-            failure: 'Something went wrong'
-        };
+     // Form
 
-        let form = document.querySelector('main-form'),
-            input = form.getElementsByTagName('input'),
-            statusMessage = document.createElement('div');
-            
-            statusMessage.classList.add('status');
+     let message = {
+        loading: 'Loading',
+        success: 'Thank you!We will contact you soon',
+        failure: 'Something went wrong'
+    };
 
-        form.addEventListener('submit',function(event){
-            event.preventDefault();
-            form.appendChild(statusMessage);
+    let form = document.querySelector('.main-form'),
+        input = form.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
 
-            let.request = new XMLHttpRequest();
-            request.open('POST','server.php','');
-            request.setRequestHeader ('Content-type', 'application/json; charset=UTF-8');
+        statusMessage.classList.add('status');
 
-            let formData = new FormData(form);
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        form.appendChild(statusMessage);
 
-            let obj = {};
-            formData.forEach(function(value, key){
-                obj[key] = value;
-            });
+        let request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+        request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
 
-            let json = JSON.stringify(obj);
+        let formData = new FormData(form);
 
-            request.send(json);
+        let obj = {};
+        formData.forEach(function(value, key) {
+            obj[key] = value;
+        });
+        let json = JSON.stringify(obj);
 
-            request.addEventListener('readystatechange', function(){
-                if(request.readyState < 4){
-                    statusMessage.innerHTML = message.loading;
+        request.send(json);
 
-                } else if(request.readyState === 4 && request.status == 200){
-                    statusMessage.innerHTML = message.success;
-                } else {
-                    statusMessage.innerHTML = message.failure;
-                }
-            });
-
-            for (let i = 0;i < input.length;i++){
-                input[i].value = '';
-
+        request.addEventListener('readystatechange', function() {
+            if (request.readyState < 4) {
+                statusMessage.innerHTML = message.loading;
+            } else if(request.readyState === 4 && request.status == 200) {
+                statusMessage.innerHTML = message.success;
+            } else {
+                statusMessage.innerHTML = message.failure;
             }
         });
 
+        for (let i = 0; i < input.length; i++) {
+            input[i].value = '';
+        }
+    });
+
+    // Slider
+
+    let slideIndex = 1,
+        slides = document.querySelectorAll('.slider-item'),
+        prev = document.querySelector('.prev'),
+        next = document.querySelector('.next'),
+        dotsWrap = document.querySelector('.slider-dots'),
+        dots = document.querySelectorAll('.dot');
+    
+    showSlides(slideIndex);
+    function showSlides(n){
+
+        if(n > slides.length){
+            slideIndex = 1;
+        }
+        if(n < 1){
+            slideIndex= slides.length;
+        }
+
+        slides.forEach((item) => item.style.display ='none');
+           // for(let i = 0;i<slides.length;i++){
+           //     slides[i].style.display= 'none';
+           // }
+
+        dots.forEach((item) => item.classList.remove('dot-active'));
+
+        slides[slideIndex - 1].style.display = 'block';
+        dots[slideIndex - 1].classList.add('dot-active');
+     }
+
+        function plusSlides(n){
+            showSlides(slideIndex += n);
+        }
+        function currentSlide(n){
+            showSlides(slideIndex = n);
+        }
+
+        prev.addEventListener('click',function(){
+            plusSlides(-1);
+        });
+        next.addEventListener('click',function(){
+            plusSlides(1);
+        });
+
+        dotsWrap.addEventListener('click',function(event){
+            for(let i = 0;i < dots.length + 1;i++){
+                if(event.target.classList.contains('dot') && event.target == dots[i -1]){
+                    currentSlide(i);
+
+                }
+            }
+        });
+        
 });
